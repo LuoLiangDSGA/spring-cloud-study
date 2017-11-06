523e9ceb055676d6acff81ff6902df1a0d2a3e01
Linag Lo <ll9420416@outlook.com>
2017-11-06 15:31:21 +0800

    add config

--- eureka-server/src/main/java/org/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/springcloud/eurekaserver/EurekaServerApplication.java
@@ -2,11 +2,18 @@ package org.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author luoliang
+ * <p>
+ * 应用入口类
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
