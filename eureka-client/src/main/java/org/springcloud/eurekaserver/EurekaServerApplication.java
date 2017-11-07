8d7cdbcc1d107d27ff82a110757e027606e1d436
Linag Lo <ll9420416@outlook.com>
2017-11-07 14:23:31 +0800

    添加服务注册项目

---
+++ eureka-client/src/main/java/org/springcloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author luoliang
+ * <p>
+ * 应用入口类
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
---
+++ eureka-client/src/main/java/org/springcloud/eurekaserver/controller/HelloController.java
@@ -0,0 +1,10 @@
+package org.springcloud.eurekaserver.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author luoliang
+ * @date 2017/11/7
+ **/
+public class HelloController {
+}
