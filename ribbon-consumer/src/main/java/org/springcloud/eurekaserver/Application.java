cf83866da79db52ee2ed9c99f6f46e4d468d0a39
Linag Lo <ll9420416@outlook.com>
2017-11-08 17:31:51 +0800

    添加Ribbon服务消费者

--- ribbon-consumer/src/main/java/org/springcloud/eurekaserver/Application.java
+++ ribbon-consumer/src/main/java/org/springcloud/eurekaserver/Application.java
@@ -2,16 +2,26 @@ package org.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author luoliang
  * <p>
  * 应用入口类
  */
-@EnableEurekaServer
+//注册为eureka客户端应用，以获取服务发现的能力
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
