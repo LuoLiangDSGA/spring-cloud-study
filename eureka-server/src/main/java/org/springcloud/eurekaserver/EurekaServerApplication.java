c1620e844205daf5ba2bfd1ee6d40b6690adfcd6
Linag Lo <ll9420416@outlook.com>
2017-11-10 15:26:55 +0800

    修改配置

--- eureka-server/src/main/java/org/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/springcloud/eurekaserver/EurekaServerApplication.java
@@ -10,6 +10,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 应用入口类
  *
  * Eureka服务注册中心，提供服务注册与发现功能
+ *
+ * 如果有多个服务注册中心，当服务提供者发送注册请求到一个服务注册中心时，他会将请求转发
+ * 给集群中相连的其他注册中心，从而实现注册中心之间的服务同步。通过服务同步，两个服务提供者
+ * 的服务信息就可以通过这两台服务注册中心的任意一台获取到
  */
 @EnableEurekaServer
 @SpringBootApplication
