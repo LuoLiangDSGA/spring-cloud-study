46aa6f18ac98b9a4c87aa089d3e5cb9f8e3f3865
Liang Lo <ll9420416@outlook.com>
2017-12-06 15:20:06 +0800

    添加api网关

---
+++ feign-consumer/src/main/java/org/springcloud/feign/Application.java
@@ -0,0 +1,23 @@
+package org.springcloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * @author luoliang
+ * <p>
+ * 消费者应用入口类
+ * <p>
+ *
+ */
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
