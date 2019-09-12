5fa8b9392c88fc493cc16fad5ba6cb04944f358a
FrankLo <ll9420416@outlook.com>
2019-09-12 18:06:13 +0800

    refactor: refactor project
    add auth service

---
+++ auth-service/src/main/java/org/springboot/cloud/web/AuthorizationController.java
@@ -0,0 +1,8 @@
+package org.springboot.cloud.web;
+
+/**
+ * @author luoliang
+ * @date 2019/9/12
+ */
+public class AuthoricationController {
+}
--- registry/src/main/java/org/springboot/cloud/RegistryApplication.java
+++ registry/src/main/java/org/springboot/cloud/RegistryApplication.java
@@ -2,12 +2,17 @@ package org.springboot.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author luoliang
+ */
 @SpringBootApplication
-public class CloudApplication {
+@EnableEurekaServer
+public class RegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
 
 }
